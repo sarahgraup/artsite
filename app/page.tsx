be115eeb8a83c225f330d23c12341c7f41a47b05
sarahgraup <sarahgraup@g.ucla.edu>
2025-02-18 11:29:08 -0800

    created masonry style

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,28 @@
 import Image from "next/image";
+'use client';
+import React from "react";
 
 export default function Home() {
+  /**
+   * loading images up
+   * images as state
+   * if images are loading use loading page
+   * if loaded, show masonry
+   * 
+   * 
+   * 
+   */
+  const [images, setImages] = React.useState()
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex max-w-6xl mx-auto px-4 py-8">
-        <h1 className="text-3xl font-bold mb-8">My Artwork</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          <div className="rounded-lg overflow-hidden shadow-md">
-            <div className="aspect-square relative">hi</div>
-            <div className="aspect-square relative">hi</div>
-            <div className="aspect-square relative">hi</div>
-          </div>
-        </div>
-      </main>
+    <div className="grid grid-col-1 lg:grid-cols-3 md:grid-cols-2 items-center min-h-screen p-8 pb-10 gap-4 sm:p-20">
+      <div className="w-full aspect-square bg-slate-500">hi col 1</div>
+      <div className="w-full aspect-square bg-slate-500">hi col 1</div>
+
+      <div className="w-full aspect-square bg-slate-500">hi col 2</div>
+      <div className="w-full aspect-square bg-slate-500">hi col 2</div>
+
+      <div className="w-full aspect-square bg-slate-500">hi col 3</div>
+      <div className="w-full aspect-square bg-slate-500">hi col 3</div>
     </div>
   );
 }
