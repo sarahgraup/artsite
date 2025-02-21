8ae3b729b24cda5df81d856b3ce17472c507a1fc
sarahgraup <sarahgraup@g.ucla.edu>
2025-02-21 11:03:34 -0800

    modified gallery page with artlist component

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,16 @@
-// import Image from "next/image";
-
-// 'use client';
+import { artworks } from './types/artwork';
+import { ArtworkCard } from "./components/ArtworkCard";
 import React from "react";
 
 export default function Home() {
-  /**
-   * loading images up
-   * images as state
-   * if images are loading use loading page
-   * if loaded, show masonry
-   * 
-   * 
-   * 
-   */
-  // const [images, setImages] = React.useState();
+
   return (
-    <div className='grid grid-col-1 lg:grid-cols-3 md:grid-cols-2 items-center min-h-screen p-8 pb-10 gap-4 sm:p-20'>
-      <div className='w-full aspect-square bg-slate-500'>hi col 1</div>
-      <div className='w-full aspect-square bg-slate-500'>hi col 1</div>
-
-      <div className='w-full aspect-square bg-slate-500'>hi col 2</div>
-      <div className='w-full aspect-square bg-slate-500'>hi col 2</div>
-
-      <div className='w-full aspect-square bg-slate-500'>hi col 3</div>
-      <div className='w-full aspect-square bg-slate-500'>hi col 3</div>
+    <div className='max-w-7xl mx-auto px-4 py-8'>
+      <div className='columns-1 sm:columns-2 lg:columns-3 gap-8'>
+        {artworks.map((artwork) => (
+          <ArtworkCard artwork={artwork} key={artwork.id} />
+        ))}
+      </div>
     </div>
   );
 }
