de0b1ecc45f5db4c7c20c4c3f129d5db14dd27bf
sarahgraup <sarahgraup@g.ucla.edu>
2025-02-21 11:03:48 -0800

    created artwork card component

---
+++ app/components/ArtworkCard.tsx
@@ -0,0 +1,22 @@
+// components/artwork-card.tsx
+import Image from "next/image";
+import { IArtwork } from "../types/artwork";
+
+export function ArtworkCard({ artwork }: { artwork: IArtwork }) {
+  return (
+    <div key={artwork.id} className='break-inside-avoid mb-8'>
+      <div className='relative rounded-xl overflow-hidden bg-white shadow-md hover:shadow-xl transition-shadow duration-300'>
+        <div className='relative w-full'>
+          <Image
+            src={artwork.src}
+            alt={artwork.title}
+            width={artwork.width}
+            height={artwork.height}
+            className='w-full h-auto'
+            priority={false}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
