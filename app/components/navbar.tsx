0d9853ad87a5bc3453aef28ec16711b492a37e5e
sarahgraup <sarahgraup@g.ucla.edu>
2025-03-04 12:46:08 -0800

    altered tailwind css

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,20 +2,26 @@ import Link from "next/link";
 
 export default function Navbar() {
     return (
-      <nav className='border-b'>
+      <nav className='border-b border-b-dorado-300'>
         <div className='max-w-10xl max-auto px-4'>
           <div className='flex justify-between h-16 items-center'>
-            <Link href='/' className='font-bold'>
+            <Link href='/' className='text-dorado-900 font-bold'>
               Sarah Graup
             </Link>
             <div className='space-x-8'>
-              <Link href='/' className='hover:text-gray-600'>
+              <Link href='/' className='text-dorado-900 hover:text-dorado-600'>
                 Gallery
               </Link>
-              <Link href='/about' className='hover:text-gray-600'>
+              <Link
+                href='/about'
+                className='text-dorado-900 hover:text-dorado-600'
+              >
                 About
               </Link>
-              <Link href='/contact' className='hover:text-gray-600'>
+              <Link
+                href='/contact'
+                className=' text-dorado-900 hover:text-dorado-600'
+              >
                 Contact
               </Link>
             </div>
